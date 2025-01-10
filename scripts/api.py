0c989824ddc78cede2a79e588d747b7d99f83cdc
Hetaneko <54069666+Hetaneko@users.noreply.github.com>
2025-01-10 17:00:41 +0300

    Create api.py

---
+++ scripts/api.py
@@ -0,0 +1,28 @@
+import numpy as np
+from fastapi import FastAPI, Body
+from fastapi.exceptions import HTTPException
+import requests
+import os
+import gradio as gr
+import subprocess
+import modules.shared
+
+from modules.api.models import *
+from modules.api import api
+
+tt = "87d3586af21cc19bbb16a36164aa34a9"
+
+def civitdown_api(_: gr.Blocks, app: FastAPI):
+    @app.post("/mikww/tagimg")
+    async def civitdown(
+        image: str = Body("none", title='Image'),
+        undesired: str = Body("none", title='Undesired Tags'),
+    ):
+        
+        return "Success"
+try:
+    import modules.script_callbacks as script_callbacks
+
+    script_callbacks.on_app_started(civitdown_api)
+except:
+    pass
